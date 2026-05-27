d81cc440736706cacec351cb752075487180274c
unknown <prajaktadeokar1996@gmail.com>
2026-05-27 23:53:08 +0530

    connection between ESP8266 and DHT22

---
+++ firmware/ESP8266_IoT.ino
@@ -0,0 +1,16 @@
+#include "DHTStable.h"
+
+#define DHT_SENSOR_PIN  D7 // The ESP8266 pin D7 connected to DHT22 sensor
+#define DHT_SENSOR_TYPE DHT22
+
+//DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
